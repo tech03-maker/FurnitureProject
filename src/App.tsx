0b949a00fe09ee7fdb57f3ec48b40e4de5c080f2
Jumah Muhamad <jumahtech@gmail.com>
2023-12-07 16:04:26 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from './components/Homepage/Home'
+// import {library} from "@fortawesome/fontawesome-svg-core"
+// import {faBars} from "@fortawesome/fontawesome-svg-core"
+// library.add(faBars)
+
+function App() {
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Homepage/Home.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./Home.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
+function Home() {
+  const [activeLink, setActiveLink] = useState("Home");
+
+  const handleNavLinkClick = (link: React.SetStateAction<string>) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Daemon<span>ex</span>
+          </a>
+          <button
+            className="navbar-toggler bg-white"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon ">
+              {/* <FontAwesomeIcon icon = {faBars} /> */}
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${activeLink === "Home" ? "active" : ""}`}
+                  aria-current="page"
+                  href="#"
+                  onClick={() => handleNavLinkClick("Home")}
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${activeLink === "Features" ? "active" : ""}`}
+                  href="#"
+                  onClick={() => handleNavLinkClick("Features")}
+                >
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${activeLink === "Pricing" ? "active" : ""}`}
+                  href="#"
+                  onClick={() => handleNavLinkClick("Pricing")}
+                >
+                  Pricing
+                </a>
+              </li>
+            </ul>
+            <span className="navbar-text">
+              <a href="#">
+                <button type="button">Contact</button>
+              </a>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
